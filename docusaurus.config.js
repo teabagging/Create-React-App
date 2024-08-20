2f0eedd451163eb843f68230e7fd15572315b2ad
543x <teabagging80@gmail.com>
2024-08-20 23:50:48 +0800

    更新 docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/teabagging/create-reacts/edit/main',
+            'https://github.com/teabagging/create-reacts/edit/main/docs',
         },
         blog: {
           showReadingTime: true,
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/teabagging/create-reacts/edit/main',
+            'https://github.com/teabagging/create-reacts/edit/main/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
